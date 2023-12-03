4f3b8ab189e9de884a5db0655d0e0ad2cef2bf36
fanatioon <fanatioon@gmail.com>
2023-12-03 17:56:43 +0300

    add index.html burger button

---
+++ coursework/assets/script/hamburger.js
@@ -0,0 +1,41 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    let closeIcon = document.querySelector(".closeIcon");
+    let menuIcon = document.querySelector(".menuIcon");
+    let menu = document.querySelector("header ul");
+    
+    // let menuItems = document.querySelectorAll("header ul li");
+
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+
+
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.addEventListener("click", () => {
+
+        if (menu.classList.contains("showMenu")) {
+            menu.classList.remove("showMenu");
+
+            closeIcon.style.display = "none";
+            menuIcon.style.display = "block";
+
+
+            menu.querySelectorAll('li').forEach( (element) => {
+                element.classList.add("small");
+                element.classList.remove("large");
+            })
+        } else {
+            menu.classList.add("showMenu");
+            closeIcon.style.display = "block";
+            menuIcon.style.display = "none";
+
+            menu.querySelectorAll('li').forEach( (element) => {
+                element.classList.remove("small");
+                element.classList.add("large");
+            })
+        }
+    });
+})
+
+
+
