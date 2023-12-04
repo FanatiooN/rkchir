ded377248694721affd464500b1a62fea6c99238
Fanil Nazirov <102851657+FanatiooN@users.noreply.github.com>
2023-12-04 16:58:33 +0300

    add favicon

--- coursework/assets/script/like.js
+++ coursework/assets/script/like.js
@@ -6,18 +6,25 @@ console.log(cardArr, typeof cardArr);
 cards.addEventListener('click', (e) => {
     let target = e.target;
     if (target.closest('.like')) {
-        let cardsArr = localStorage.getItem('cardsArr') || [];
+        let likedCards = localStorage.getItem('likedCards') || [];
         let cardId = target.parentElement.parentElement.parentElement;
-        console.log(cardArr, cardId, cardArr.indexOf(cardId));
-        
+
+        console.log(likedCards, typeof likedCards);
+
+
         if (target.closest('.like').classList.contains('liked'))
         {
             target.closest('.like').classList.remove('liked');
+            likedCards.remove(likedCards.indexOf(cardId));
+            localStorage.setItem('likedCards', likedCards);
         }
         else
         {
             target.closest('.like').classList.add('liked');
+            likedCards.push(cardId);
+            localStorage.setItem('likedCards', likedCards);
         }
         
+        console.log(cardArr, cardArr.length);
     }
 });
