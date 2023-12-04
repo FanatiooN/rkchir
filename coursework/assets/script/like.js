19242ed9d00a1f58127e22c0d4dfa9bbc5596263
Fanil Nazirov <102851657+FanatiooN@users.noreply.github.com>
2023-12-04 16:44:02 +0300

    special for ns

--- coursework/assets/script/like.js
+++ coursework/assets/script/like.js
@@ -1,8 +1,15 @@
 const cards = document.querySelector('.cards');
+let cardArr = Array(cards.querySelectorAll('.card'));
+console.log(cardArr, typeof cardArr);
+
 
 cards.addEventListener('click', (e) => {
     let target = e.target;
     if (target.closest('.like')) {
+        let cardsArr = localStorage.getItem('cardsArr') || [];
+        let cardId = target.parentElement.parentElement.parentElement;
+        console.log(cardArr, cardId, cardArr.indexOf(cardId));
+        
         if (target.closest('.like').classList.contains('liked'))
         {
             target.closest('.like').classList.remove('liked');
@@ -11,6 +18,6 @@ cards.addEventListener('click', (e) => {
         {
             target.closest('.like').classList.add('liked');
         }
-        console.log('click');
+        
     }
 });
