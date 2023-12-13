97b7f11746c28c434ede46963966cce0033d0726
Fanil Nazirov <102851657+FanatiooN@users.noreply.github.com>
2023-12-13 22:44:05 +0300

    add gallery prac14

--- prac14/assets/script/index.js
+++ prac14/assets/script/index.js
@@ -1,1 +1,16 @@
+let gallery_side = document.querySelector('.gallery_side')
+let main_img = document.querySelector('.main_img')
+let gallery = document.querySelector('.gallery')
 
+gallery_side.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target.closest('img')) {
+        let element = document.createElement('img');
+        
+        element.src = target.src;
+        
+        main_img.innerHTML = ''
+        main_img.appendChild(element)
+
+    }
+})
